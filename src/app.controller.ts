7c614cc381fcc2343dbfe2af99266e4520374676
Török Donát <td.zazsa@gmail.com>
2026-09-23 10:20:00 +0200

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Render } from '@nestjs/common';
+import { AppService } from './app.service.js';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('index')
+  getHello() {
+    return {
+      title: 'My First NestJS App'
+    }
+  }
+}
